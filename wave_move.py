dada84bbea42974e8f68a31052e8ed99c1b185fc
Jaster4000a <jasoncabrejos@hotmail.com>
2022-12-16 09:26:22 -0600

    Add files via upload

--- wave_move.py
+++ wave_move.py
@@ -16,17 +16,7 @@ def playback():
         waypointz=line[:-1].split('},')
         gripper_states=ast.literal_eval("(" + waypointz[2])
         w=(ast.literal_eval(waypointz[0][1:]+"}"),ast.literal_eval(waypointz[1][1:]+"}"),gripper_states[0],gripper_states[1])
-        #print(w)
-        #print((gripper_states[0],gripper_states[1]))
-        #waypoint[0]=json.loads(waypoint[0])
-        #print(waypoint)
-        #print("\n")
-        #print(w[0])
-        #print(w[1])
-        #print(w[2])
-        #print(w[3])
         _waypoints.append(w)
-        #input("ENTER TO CONTINUE")
 
     f.close() 
 
@@ -92,7 +82,4 @@ if __name__ == '__main__':
     rospy.init_node('Wave',anonymous=True)
     playback()
     # Initialze ROS node
-    # Subscribe to head_camera image topic
-    #playback_thread=threading.Thread(target=playback,args=("movement1.txt",))
-    #rospy.Subscriber('can_states', String, playback,queue_size=1)
     rospy.spin()
